e65154792ba981deec870efb3a26e8124c5be5eb
Iteneux <damian.kowalewski88@gmail.com>
2016-03-14 23:05:29 +0100

    Qualifications knockout

--- src/iteneux/competition/Qualification.java
+++ src/iteneux/competition/Qualification.java
@@ -16,49 +16,7 @@ public class Qualification extends Competition {
 		this.setRules(rules);
 	}
 
-	public void draw() {
-		/*if (teams.size() > 1) {
-			for (int i = 0; i < teams.size() / 2; i++) {
-				Match m = new Match();
-		
-				m.setHomeTeam(teams.get(i));
-				m.setAwayTeam(teams.get(teams.size() - i - 1));
-		
-				round.addGame(m);
-			}
-		
-		}*/
-	}
-
-	public void playQualifications() {
-
-		/*for (Match m : round.getMatches()) {
-		
-			Random rnd = new Random();
-		
-			boolean isHomeWinner = Simulation.getWinner(m.getHomeTeam(), m.getAwayTeam());
-			int homeScore = 0;
-			int awayScore = 0;
-		
-			if (isHomeWinner) {
-				homeScore = rnd.nextInt(6) + 1;
-				awayScore = rnd.nextInt(homeScore);
-			} else {
-				awayScore = rnd.nextInt(6) + 1;
-				homeScore = rnd.nextInt(awayScore);
-			}
-			m.setHomeTeamScore(homeScore);
-			m.setAwayTeamScore(awayScore);
-		
-			if (m.getHomeTeamScore() > m.getAwayTeamScore()) {
-				teams.add(m.getHomeTeam());
-			} else {
-				teams.add(m.getAwayTeam());
-			}
-		}*/
-	}
-
-	public void playAndDraw() {
+	public List<Team> playAndDraw() {
 		for (QualificationRule rule : rules) {
 			Round round = new Round();
 
@@ -66,19 +24,19 @@ public class Qualification extends Competition {
 			System.out.println("roundTeams: " + roundTeams.size());
 
 			for (int i = 0; i < rule.getTeamNumber(); i = i + 2) {
-				round.addGame(new Match(roundTeams.get(i), roundTeams.get(i+1)));
+				round.addGame(new Match(roundTeams.get(i), roundTeams.get(i + 1)));
 			}
-			
+
 			rounds.add(round);
 			List<Team> outTeams = new ArrayList<Team>();
 			for (Match m : round.getMatches()) {
-				
+
 				Random rnd = new Random();
-			
+
 				boolean isHomeWinner = Simulation.getWinner(m.getHomeTeam(), m.getAwayTeam());
 				int homeScore = 0;
 				int awayScore = 0;
-			
+
 				if (isHomeWinner) {
 					homeScore = rnd.nextInt(6) + 1;
 					awayScore = rnd.nextInt(homeScore);
@@ -88,24 +46,23 @@ public class Qualification extends Competition {
 				}
 				m.setHomeTeamScore(homeScore);
 				m.setAwayTeamScore(awayScore);
-			
+
 				if (m.getHomeTeamScore() > m.getAwayTeamScore()) {
 					outTeams.add(m.getHomeTeam());
 				} else {
 					outTeams.add(m.getAwayTeam());
 				}
 			}
-			
+
 			List<Team> inTeams = teams.subList(rule.getTeamNumber(), teams.size());
 			System.out.println("outTeams: " + outTeams.size());
-			
+
 			teams = new ArrayList<Team>();
 			teams.addAll(outTeams);
 			teams.addAll(inTeams);
-			
-			System.out.println(teams.size());
-			
 		}
+		
+		return teams;
 	}
 
 	/*public void show() {
--- src/iteneux/sim/Sim.java
+++ src/iteneux/sim/Sim.java
@@ -19,7 +19,8 @@ public class Sim {
 	static final LoggerWrapper loggerWrapper = LoggerWrapper.getInstance();
 
 	public static void main(String[] args) {
-		List<Team> teams = new ArrayList<Team>();
+		List<Team> championsTeams = new ArrayList<Team>();
+		List<Team> nonChampionsTeams = new ArrayList<Team>();
 
 		/* Test
 		teams.add(new Team("Chelsea", 10));
@@ -158,87 +159,96 @@ public class Sim {
 		 * 
 		 */
 
-		/*teams.add(new ClubTeam("Valencia CF", 10, 10, Association.ESP));
-		teams.add(new ClubTeam("Bayer Leverkusen", 10, 10, Association.GER));
-		teams.add(new ClubTeam("Sporting CP", 10, 10, Association.POR));
-		teams.add(new ClubTeam("Manchester United", 10, 10, Association.ENG));
-		teams.add(new ClubTeam("SS Lazio", 10, 10, Association.ITA));
+		nonChampionsTeams.add(new ClubTeam("Valencia CF", 10, 10, Association.ESP));
+		nonChampionsTeams.add(new ClubTeam("Bayer Leverkusen", 10, 10, Association.GER));
+		nonChampionsTeams.add(new ClubTeam("Sporting CP", 10, 10, Association.POR));
+		nonChampionsTeams.add(new ClubTeam("Manchester United", 10, 10, Association.ENG));
+		nonChampionsTeams.add(new ClubTeam("SS Lazio", 10, 10, Association.ITA));
 
-		teams.add(new ClubTeam("AS Monaco", 10, 10, Association.FRA));
-		teams.add(new ClubTeam("Club Brugge", 10, 10, Association.BEL));
-		teams.add(new ClubTeam("Rapid Wiedeń", 10, 10, Association.AUT));
-		teams.add(new ClubTeam("CSKA Moskwa", 10, 10, Association.RUS));
-		teams.add(new ClubTeam("Fenerbahçe SK", 10, 10, Association.TUR));
-		teams.add(new ClubTeam("Sparta Praga", 10, 10, Association.CZE));
-		teams.add(new ClubTeam("Ajax", 10, 10, Association.NED));
-		teams.add(new ClubTeam("Panathinaikos AO", 10, 10, Association.GRE));
-		teams.add(new ClubTeam("Szachtar Donieck", 10, 10, Association.UKR));
-		teams.add(new ClubTeam("BSC Young Boys", 10, 10, Association.SUI));*/
+		nonChampionsTeams.add(new ClubTeam("AS Monaco", 10, 10, Association.FRA));
+		nonChampionsTeams.add(new ClubTeam("Club Brugge", 10, 10, Association.BEL));
+		nonChampionsTeams.add(new ClubTeam("Rapid Wiedeń", 10, 10, Association.AUT));
+		nonChampionsTeams.add(new ClubTeam("CSKA Moskwa", 10, 10, Association.RUS));
+		nonChampionsTeams.add(new ClubTeam("Fenerbahçe SK", 10, 10, Association.TUR));
+		nonChampionsTeams.add(new ClubTeam("Sparta Praga", 10, 10, Association.CZE));
+		nonChampionsTeams.add(new ClubTeam("Ajax", 10, 10, Association.NED));
+		nonChampionsTeams.add(new ClubTeam("Panathinaikos AO", 10, 10, Association.GRE));
+		nonChampionsTeams.add(new ClubTeam("Szachtar Donieck", 10, 10, Association.UKR));
+		nonChampionsTeams.add(new ClubTeam("BSC Young Boys", 10, 10, Association.SUI));
 		
 		
 		
 		//3 + 17 = 20 ->10
-		teams.add(new ClubTeam("FC Basel", 10, 10, Association.SUI));
-		teams.add(new ClubTeam("Viktoria Pilzno", 10, 10, Association.CZE));
-		teams.add(new ClubTeam("Red Bull Salzburg", 10, 10, Association.AUT));
+		championsTeams.add(new ClubTeam("FC Basel", 10, 10, Association.SUI));
+		championsTeams.add(new ClubTeam("Viktoria Pilzno", 10, 10, Association.CZE));
+		championsTeams.add(new ClubTeam("Red Bull Salzburg", 10, 10, Association.AUT));
 
 		//30 + 4 = 34 -> 17
-		teams.add(new ClubTeam("Steaua Bukareszt", 10, 10, Association.ROU));
-		teams.add(new ClubTeam("Malmö FF", 10, 10, Association.SWE));
-		teams.add(new ClubTeam("Qarabağ Ağdam", 10, 10, Association.AZE));
-		teams.add(new ClubTeam("Skënderbeu Korcza", 10, 10, Association.ALB));
-		teams.add(new ClubTeam("Maccabi Tel Awiw", 10, 10, Association.ISR));
-		teams.add(new ClubTeam("Łudogorec Razgrad", 10, 10, Association.BUL));
-		teams.add(new ClubTeam("Žalgiris Wilno", 10, 10, Association.LTU));
-		teams.add(new ClubTeam("APOEL Nikozja", 10, 10, Association.CYP));
-		teams.add(new ClubTeam("Dila Gori", 10, 10, Association.GEO));
-		teams.add(new ClubTeam("Molde FK", 10, 10, Association.NOR));
-		teams.add(new ClubTeam("FK Astana", 10, 10, Association.KAZ));
-		teams.add(new ClubTeam("Wardar Skopje", 10, 10, Association.MKD));
-		teams.add(new ClubTeam("FC Midtjylland", 10, 10, Association.DEN));
-		teams.add(new ClubTeam("Partizan Belgrad", 10, 10, Association.SRB));
-		teams.add(new ClubTeam("Videoton FC", 10, 10, Association.HUN));
-		teams.add(new ClubTeam("Milsami Orgiejów", 10, 10, Association.MDA));
-		teams.add(new ClubTeam("FK Sarajevo", 10, 10, Association.BIH));
-		teams.add(new ClubTeam("Dundalk F.C.", 10, 10, Association.IRL));
-		teams.add(new ClubTeam("Dinamo Zagrzeb", 10, 10, Association.CRO));
-		teams.add(new ClubTeam("HJK Helsinki", 10, 10, Association.FIN));
-		teams.add(new ClubTeam("Fola Esch", 10, 10, Association.LUX));
-		teams.add(new ClubTeam("Lech Poznań", 10, 10, Association.POL));
-		teams.add(new ClubTeam("NK Maribor", 10, 10, Association.SVN));
-		teams.add(new ClubTeam("Stjarnan", 10, 10, Association.ISL));
-		teams.add(new ClubTeam("Hibernians", 10, 10, Association.MLT));
-		teams.add(new ClubTeam("BATE Borysów", 10, 10, Association.BLR));
-		teams.add(new ClubTeam("FK AS Trenčín", 10, 10, Association.SVK));
-		teams.add(new ClubTeam("FK Ventspils", 10, 10, Association.LVA));
-		teams.add(new ClubTeam("Rudar Pljevlja", 10, 10, Association.MNE));
-		teams.add(new ClubTeam("Celtic", 10, 10, Association.SCO));
+		championsTeams.add(new ClubTeam("Steaua Bukareszt", 10, 10, Association.ROU));
+		championsTeams.add(new ClubTeam("Malmö FF", 10, 10, Association.SWE));
+		championsTeams.add(new ClubTeam("Qarabağ Ağdam", 10, 10, Association.AZE));
+		championsTeams.add(new ClubTeam("Skënderbeu Korcza", 10, 10, Association.ALB));
+		championsTeams.add(new ClubTeam("Maccabi Tel Awiw", 10, 10, Association.ISR));
+		championsTeams.add(new ClubTeam("Łudogorec Razgrad", 10, 10, Association.BUL));
+		championsTeams.add(new ClubTeam("Žalgiris Wilno", 10, 10, Association.LTU));
+		championsTeams.add(new ClubTeam("APOEL Nikozja", 10, 10, Association.CYP));
+		championsTeams.add(new ClubTeam("Dila Gori", 10, 10, Association.GEO));
+		championsTeams.add(new ClubTeam("Molde FK", 10, 10, Association.NOR));
+		championsTeams.add(new ClubTeam("FK Astana", 10, 10, Association.KAZ));
+		championsTeams.add(new ClubTeam("Wardar Skopje", 10, 10, Association.MKD));
+		championsTeams.add(new ClubTeam("FC Midtjylland", 10, 10, Association.DEN));
+		championsTeams.add(new ClubTeam("Partizan Belgrad", 10, 10, Association.SRB));
+		championsTeams.add(new ClubTeam("Videoton FC", 10, 10, Association.HUN));
+		championsTeams.add(new ClubTeam("Milsami Orgiejów", 10, 10, Association.MDA));
+		championsTeams.add(new ClubTeam("FK Sarajevo", 10, 10, Association.BIH));
+		championsTeams.add(new ClubTeam("Dundalk F.C.", 10, 10, Association.IRL));
+		championsTeams.add(new ClubTeam("Dinamo Zagrzeb", 10, 10, Association.CRO));
+		championsTeams.add(new ClubTeam("HJK Helsinki", 10, 10, Association.FIN));
+		championsTeams.add(new ClubTeam("Fola Esch", 10, 10, Association.LUX));
+		championsTeams.add(new ClubTeam("Lech Poznań", 10, 10, Association.POL));
+		championsTeams.add(new ClubTeam("NK Maribor", 10, 10, Association.SVN));
+		championsTeams.add(new ClubTeam("Stjarnan", 10, 10, Association.ISL));
+		championsTeams.add(new ClubTeam("Hibernians", 10, 10, Association.MLT));
+		championsTeams.add(new ClubTeam("BATE Borysów", 10, 10, Association.BLR));
+		championsTeams.add(new ClubTeam("FK AS Trenčín", 10, 10, Association.SVK));
+		championsTeams.add(new ClubTeam("FK Ventspils", 10, 10, Association.LVA));
+		championsTeams.add(new ClubTeam("Rudar Pljevlja", 10, 10, Association.MNE));
+		championsTeams.add(new ClubTeam("Celtic", 10, 10, Association.SCO));
 
 		
-		//8 -> 4
-		teams.add(new ClubTeam("Crusaders F.C", 10, 10, Association.NIR));
-		teams.add(new ClubTeam("Piunik Erywań", 10, 10, Association.ARM));
-		teams.add(new ClubTeam("B36 Tórshavn", 10, 10, Association.FRO));
-		teams.add(new ClubTeam("FC Santa Coloma", 10, 10, Association.AND));
-		teams.add(new ClubTeam("The New Saints F.C.", 10, 10, Association.WAL));
-		teams.add(new ClubTeam("Levadia Tallinn", 10, 10, Association.EST));
-		teams.add(new ClubTeam("SS Folgore/Falciano", 10, 10, Association.SMR));
-		teams.add(new ClubTeam("Lincoln Red Imps", 10, 10, Association.GIB));
+		//8 + 0-> 4
+		championsTeams.add(new ClubTeam("Crusaders F.C", 10, 10, Association.NIR));
+		championsTeams.add(new ClubTeam("Piunik Erywań", 10, 10, Association.ARM));
+		championsTeams.add(new ClubTeam("B36 Tórshavn", 10, 10, Association.FRO));
+		championsTeams.add(new ClubTeam("FC Santa Coloma", 10, 10, Association.AND));
+		championsTeams.add(new ClubTeam("The New Saints F.C.", 10, 10, Association.WAL));
+		championsTeams.add(new ClubTeam("Levadia Tallinn", 10, 10, Association.EST));
+		championsTeams.add(new ClubTeam("SS Folgore/Falciano", 10, 10, Association.SMR));
+		championsTeams.add(new ClubTeam("Lincoln Red Imps", 10, 10, Association.GIB));
 		
-		Collections.reverse(teams);
+		Collections.reverse(championsTeams);
 		
-		System.out.println(teams.size());
+		System.out.println(championsTeams.size());
 		
-		List<QualificationRule> rules = new ArrayList<QualificationRule>();
+		List<QualificationRule> rulesC = new ArrayList<QualificationRule>();
 		
-		rules.add(new QualificationRule(Phase.KNOCKOUT, 8));
-		rules.add(new QualificationRule(Phase.KNOCKOUT, 34));
-		rules.add(new QualificationRule(Phase.KNOCKOUT, 20));
+		rulesC.add(new QualificationRule(Phase.KNOCKOUT, 8));
+		rulesC.add(new QualificationRule(Phase.KNOCKOUT, 34));
+		rulesC.add(new QualificationRule(Phase.KNOCKOUT, 20));
+		rulesC.add(new QualificationRule(Phase.KNOCKOUT, 10));
 
-		Qualification nonChampions = new Qualification(teams, rules);
-		nonChampions.playAndDraw();
-		nonChampions.draw();
-		nonChampions.playQualifications();
+		Qualification championsRoute = new Qualification(championsTeams, rulesC);
+		championsTeams = championsRoute.playAndDraw();
+		System.out.println(championsTeams);
+		
+		List<QualificationRule> rulesNC = new ArrayList<QualificationRule>();
+		
+		rulesNC.add(new QualificationRule(Phase.KNOCKOUT, 10));
+		rulesNC.add(new QualificationRule(Phase.KNOCKOUT, 10));
+
+		Qualification nonChampionsRoute = new Qualification(nonChampionsTeams, rulesNC);
+		nonChampionsTeams = nonChampionsRoute.playAndDraw();
+		System.out.println(nonChampionsTeams);
 		//nonChampions.show();
 	}
 }
